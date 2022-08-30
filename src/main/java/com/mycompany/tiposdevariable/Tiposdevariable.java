6f2adcef9694ee4cf176c93b842c3caa5beaff10
[jmbbenavides] <[1942395@clases.edu.sv]>
2022-08-30 04:13:05 +0000

    Mi primewr commit

---
+++ src/main/java/com/mycompany/tiposdevariable/Tiposdevariable.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tiposdevariable;
+
+/**
+ *
+ * @author jmbb
+ */
+public class Tiposdevariable {
+
+    public static void main(String[] args) {
+     // Declarar la variable speed de tipo int (numeros enteros)
+     int spped;
+     
+     //Actualizar el contrario de la variable speed:
+     spped = 10; //si ya habias declarado la varible
+     
+     // Declarar la varible y asignarle un valor al mimo tiempo:
+     int salary = 1000;
+     
+     // Crear una variable de tipo String:
+     String eployeeName = "Anahi Salgado";
+     
+    }
+}
